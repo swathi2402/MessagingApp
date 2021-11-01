824408f38f855e6021719743c11874a505d097b1
Swathi <swathi.h@perfios.com>
2021-11-01 19:48:59 +0530

    REFACTORED: Modified to display message using curl

--- src/main/java/com/brigdelabz/messagingapp/controller/GreetingController.java
+++ src/main/java/com/brigdelabz/messagingapp/controller/GreetingController.java
@@ -3,20 +3,18 @@ package com.brigdelabz.messagingapp.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brigdelabz.messagingapp.model.Greeting;
 
 @RestController
-@RequestMapping("/greeting")
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/")
+	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
--- src/main/java/com/brigdelabz/messagingapp/model/Greeting.java
+++ src/main/java/com/brigdelabz/messagingapp/model/Greeting.java
@@ -1,30 +1,22 @@
 package com.brigdelabz.messagingapp.model;
 
 public class Greeting {
-	
-	private long UserId;
-	private String userName;
+
+	private final long userId;
+	private final String userName;
 
 	public Greeting(long userId, String userName) {
 		super();
-		UserId = userId;
+		this.userId = userId;
 		this.userName = userName;
 	}
 
 	public long getUserId() {
-		return UserId;
+		return userId;
 	}
-
-	public void setUserId(long userId) {
-		UserId = userId;
-	}
-
 
 	public String getUserName() {
 		return userName;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
 }
